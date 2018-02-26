626215d76e90f1dc0b20a04e55cabb0b2df3e409
AntonAtrashkou <alivarya11@gmail.com>
2018-02-26 21:59:31 +0300

    Exercise N7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function getZerosCount(number) {
+  var uns = 0,
+  i = 5;
+  
+  while (i <= number) {
+    var val = Math.floor(number / i);
+    uns += val;
+    i *=5; 
+  }
+  return uns;
   // your implementation
 }
+  // for (var i = 5; i <= number; i * 5) {
+  //   var val = Math.floor(number / i);
+  //   // var rem = number % i;
+  //   // var val1 = (number - rem) / i;
+  //   uns += val;
+  // }
